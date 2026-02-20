617a7270da6f551ad884e0453538c20bb332ca5c
AbrahamGlzP <abrahamglzp@gmail.com>
2026-02-19 23:40:44 -0600

    Initial Commit

---
+++ AsyncStreamVSCombineDemo/AsyncStreamVSCombineDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncStreamVSCombineDemoApp.swift
+//  AsyncStreamVSCombineDemo
+//
+//  Created by Abraham Gonzalez Puga on 19/02/26.
+//
+
+import SwiftUI
+
+@main
+struct AsyncStreamVSCombineDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
